6334043b883591df19e753151ef1f593dcaa3fd5
ninge <nate8983@gmail.com>
2018-02-05 15:04:17 -0800

    All for you, Ray

--- sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/MainActivity.java
+++ sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/MainActivity.java
@@ -14,12 +14,19 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setTheme(android.R.style.Theme_Material_NoActionBar_Fullscreen);
+
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = (TextView) findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
     }
+
+    //Intent register = new Intent(this, RegisterActivity.class);
+    //startIntent(register);
+
+    // LoginActivity
+
+    // GoogleMapsActivity
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
