786606a6894bf06f290d87ab86b37682e50e9f62
unknown <rmuhlenkamp@umail.ucsb.edu>
2018-02-11 17:33:32 -0800

    Continuing work on creating spots

--- sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/CreateSpotEntry.java
+++ sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/CreateSpotEntry.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.EditText;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -17,6 +19,7 @@ import java.util.List;
 public class CreateSpotEntry extends AppCompatActivity {
 
     EditText addressInput;
+    EditText rateInput;
 
     private DatabaseReference spotDatabase;
 
@@ -26,19 +29,19 @@ public class CreateSpotEntry extends AppCompatActivity {
         setContentView(R.layout.activity_create_spot_entry);
 
         addressInput = (EditText) findViewById(R.id.addressEditText);
-
+        rateInput = (EditText) findViewById(R.id.rateEditText);
     }
 
     public void submitSpotButtonTapped(View view) {
         String address = addressInput.getText().toString();
+        double rate = Double.parseDouble(rateInput.getText().toString());
 
         spotDatabase = FirebaseDatabase.getInstance().getReference();
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        ParkingSpot newSpot = new ParkingSpot("user1+2+3", address, 12.50);
-
-        SpotCheckUser newUser = new SpotCheckUser("rick_franc", "Rick Franc");
-
-        spotDatabase.child("users").child(newUser.getUserID()).setValue(newUser);
+        ParkingSpot newSpot = new ParkingSpot(user.getUid(), address, rate);
+//        SpotCheckUser newUser = new SpotCheckUser("rick_franc", "Rick Franc");
+//        spotDatabase.child("users").child(newUser.getUserID()).setValue(newUser);
 
         spotDatabase.child("parking_spots").child(newSpot.getSpotID()).setValue(newSpot);
     }
