b66c0d8c9e3f0143b86bf24a44fdf75079544c18
Kai <labasankai3@gmail.com>
2018-02-13 14:30:30 -0800

    Added the java classes. Next commit will be finished

---
+++ sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/ProfilePage.java
@@ -0,0 +1,16 @@
+package com.ucsb.cs48.spotcheck;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by labas on 2/13/2018.
+ */
+
+public class ProfilePage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_page);
+    }
+}
