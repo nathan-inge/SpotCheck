53162d1f6d4ffa3910f265ef4479ec3b73017398
ninge <nate8983@gmail.com>
2018-02-18 21:44:06 -0800

    Working on updating user's name

--- sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/EditProfile.java
+++ sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/EditProfile.java
@@ -1,15 +1,36 @@
 package com.ucsb.cs48.spotcheck;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+
+import com.ucsb.cs48.spotcheck.SCFirebaseInterface.SCFirebase;
+import com.ucsb.cs48.spotcheck.SCLocalObjects.SpotCheckUser;
 
 
 public class EditProfile extends AppCompatActivity {
+
+    SpotCheckUser user;
+    EditText editName;
+    SCFirebase scFirebase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        scFirebase = new SCFirebase();
+
+        Intent intent = getIntent();
+        user = intent.getParcelableExtra("currentSCUser");
+
+        editName = findViewById(R.id.edit_user_name);
+
+        if(user != null) {
+            editName.setHint(user.getFullname());
+        }
     }
 
     public void cancel(View view){
@@ -18,6 +39,12 @@ public class EditProfile extends AppCompatActivity {
 
     public void confirm(View view){
         //Change user's screen name, profile pic, and location in firebase
+        String rawNewName = editName.getText().toString();
+
+        if((rawNewName.length()) > 0) {
+            scFirebase.uploadUser(user);
+        }
+
         finish();
     }
 }
--- sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/ProfilePage.java
+++ sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/ProfilePage.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
@@ -58,6 +59,7 @@ public class ProfilePage extends AppCompatActivity {
 
     public void editProfile(View view){
         Intent intent = new Intent(this, EditProfile.class);
+        intent.putExtra("currentSCUser", (Parcelable) user);
         startActivity(intent);
     }
 
