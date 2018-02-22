dc99f0a48df7e7aa6c3401b886ef7f2735acebc8
Juan <juanmanzo@umail.ucsb.edu>
2018-02-22 12:53:24 -0800

    SpotDetails

--- sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/SpotDetailsActivity.java
+++ sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/SpotDetailsActivity.java
@@ -1,8 +1,25 @@
 package com.ucsb.cs48.spotcheck;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Parcelable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class SpotDetailsActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spot_detail);
+    }
 
+    public void rent(View view){
+
+    }
 
 
 
