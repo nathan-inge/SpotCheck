3704b9e0339a8a33441e8622744c056238f4d917
Kai <labasankai3@gmail.com>
2018-03-12 17:39:34 -0700

    added changed MyParkingSpots.java

--- sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/MyParkingSpots.java
+++ sc_android/app/src/main/java/com/ucsb/cs48/spotcheck/MyParkingSpots.java
@@ -6,6 +6,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.ProgressBar;
@@ -19,6 +20,8 @@ import com.ucsb.cs48.spotcheck.SCLocalObjects.ParkingSpot;
 import com.ucsb.cs48.spotcheck.SCLocalObjects.SpotCheckUser;
 
 import java.util.ArrayList;
+
+import static com.ucsb.cs48.spotcheck.Utilities.SCConstants.REQUEST_SPOT_DETAILS;
 
 public class MyParkingSpots extends AppCompatActivity {
 
@@ -55,6 +58,7 @@ public class MyParkingSpots extends AppCompatActivity {
                if(data != null) {
                    usersParkingSpots = data;
 
+
                    final Handler mainHandler = new Handler(Looper.getMainLooper());
                    mainHandler.post(new Runnable() {
                        @Override
@@ -65,20 +69,22 @@ public class MyParkingSpots extends AppCompatActivity {
                             * with the usersParkingSpots
                             *
                             */
+                           String[] allSpots = new String[usersParkingSpots.size()];
+                           for (int i=0; i < usersParkingSpots.size(); i++)
+                               allSpots[i] = usersParkingSpots.get(i).getAddress();
+                           ArrayAdapter adapter = new ArrayAdapter<String>(MyParkingSpots.this,
+                                   R.layout.activity_listview, allSpots);
+                           ListView listView = (ListView) findViewById(R.id.my_parking_spots);
+                           listView.setAdapter(adapter);
                        }
                    });
-
                }
-
             }
         });
-
-//        ArrayAdapter adapter = new ArrayAdapter<String>(this,
-//                R.layout.activity_listview, user.getCurrentListings());
-//
-//        ListView listView = (ListView) findViewById(R.id.my_parking_spots);
-//        listView.setAdapter(adapter);
-
-
+    }
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Intent i = new Intent(getApplicationContext(), SpotDetailActivity.class);
+        i.putExtra("spotID", usersParkingSpots.get(position-1).getSpotID());
+        startActivityForResult(i, REQUEST_SPOT_DETAILS);
     }
 }
